0cfb6f2123a72fbf7cc895785228552f9839656b
Marcin Pieczka <pieczka.marcin@gmail.com>
2018-12-28 23:34:20 +0100

    starts to work a bit

--- cht.py
+++ cht.py
@@ -2,6 +2,8 @@
 First make HT without resizing of buckets
 """
 import ctypes as ct
+from pprint import pformat
+
 import pudb
 
 
@@ -17,62 +19,72 @@ class HashTable:
             _fields_ = [
                 ('data_len', ct.c_uint16),
                 ('array_size', ct.c_uint16),
-                ('data', ct.POINTER(ct.POINTER(self.data_struct)))
+                ('indexes', ct.POINTER(ct.c_uint64))
             ]
 
         self.bucket_struct = BucketStruct
 
         self.data_def = data_def
         self.key_type = key_type
-        self.bucket_count = ct.c_uint64(initial_buckets_count)
-        self.len = ct.c_uint64(0)
-        self.buckets = (ct.POINTER(self.bucket_struct) * self.bucket_count.value)()
+        self.bucket_count = initial_buckets_count
+        self.data_slots = initial_buckets_count
+        self.len = 0
+        self.data = (ct.POINTER(self.data_struct) * self.data_slots)()
+        self.buckets = (ct.POINTER(self.bucket_struct) * self.bucket_count)()
 
     def __setitem__(self, key, values):
-        self.len = ct.c_uint64(self.len.value + 1)
         key_hash = hash(key)
-        d_struct = self.data_struct(
+        bucket_index = self._bucket_index(key_hash)
+
+        data_struct = self.data_struct(
             cht_key_hash=ct.c_int64(key_hash),
             cht_key=self.key_type(key),
             **{k: self.data_def[k](v) for k, v in values.items()},
         )
-        bucket_index = self._bucket_index(key_hash)
+
+        if self.buckets[bucket_index]:
+            for i in range(self.buckets[bucket_index].contents.data_len):
+                data_index = self.buckets[bucket_index].contents.indexes[i]
+                if self.data[data_index].contents.cht_key == key:
+                    self.data[data_index] = ct.pointer(data_struct)
+                    return
+
+        if self.len < self.data_slots:
+            self.data[self.len] = ct.pointer(data_struct)
+        else:
+            raise Exception('Resizing not implemented')
+        data_index = ct.c_uint64(self.len)
+        self.len += 1
+
         if not self.buckets[bucket_index]:
-            darray = (ct.POINTER(self.data_struct) * 1)()
-            darray[0] = ct.pointer(d_struct)
             self.buckets[bucket_index] = ct.pointer(self.bucket_struct(
                 data_len=ct.c_uint16(1),
                 array_size=ct.c_uint16(1),
-                data=ct.cast(darray, ct.POINTER(ct.POINTER(self.data_struct))),
+                indexes=ct.pointer(data_index),
             ))
         else:
-            #pudb.set_trace()
             bucket = self.buckets[bucket_index].contents
-            old_size = bucket.array_size
-            old_len = bucket.data_len
-            resized = (ct.POINTER(self.data_struct) * (old_size + 1))(bucket.data.contents)
-            resized[old_len] = ct.pointer(d_struct)
-            for p in resized:
-                if p:
-                    print(self._getdict(p.contents))
-            bucket.data = ct.pointer(ct.cast(resized, ct.POINTER(self.data_struct)))
-            bucket.array_size = ct.c_uint16(old_size + 1)
-            bucket.data_len = ct.c_uint16(old_len + 1)
+
+            array_size = bucket.array_size
+            data_len = bucket.data_len
+            if data_len < array_size:
+                bucket.contents.indexes[data_len] = data_struct
+            else:
+                resized = (ct.POINTER(ct.c_uint64) * (array_size + 1))(bucket.indexes)
+                resized[data_len] = ct.pointer(data_index)
+                bucket.data = ct.cast(resized, ct.POINTER(self.data_struct))
+                bucket.array_size = ct.c_uint16(array_size + 1)
+            bucket.data_len = ct.c_uint16(data_len + 1)
 
     def __getitem__(self, key):
-        #pudb.set_trace()
         bucket_index = self._bucket_index(hash(key))
         bucket = self.buckets[bucket_index]
-        if not bucket:
-            raise KeyError()
-        data = bucket.contents.data.contents
-        for i in range(bucket.contents.data_len):
-            data = bucket.contents.data.contents[i * ct.sizeof(self.data_struct)]
-            if data.cht_key == key:
-                return self._getdict(data)
-            else:
-                print(data.cht_key)
-        raise KeyError()  # change so that error wil show the key
+        if bucket:
+            for i in range(bucket.contents.data_len):
+                data_index = bucket.contents.indexes[i]
+                if self.data[data_index].contents.cht_key == key:
+                    return self._getdict(self.data[data_index].contents)
+        raise KeyError()
 
     def get(self, key, default=None):
         try:
@@ -81,10 +93,25 @@ class HashTable:
             return default
 
     def _bucket_index(self, key_hash):
-        return key_hash % self.bucket_count.value
+        return key_hash % self.bucket_count
 
     def _getdict(self, struct):
         return dict((field, getattr(struct, field)) for field, _ in struct._fields_)
 
     def __len__(self):
-        return self.len.value
+        return self.len
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        entry_nr = len(self) if len(self) < 20 else 20
+        entries = {}
+        for i in range(entry_nr):
+            d = self._getdict(self.data[i].contents)
+            key = d['cht_key']
+            del d['cht_key']
+            del d['cht_key_hash']
+            entries[key] = d
+        return f"HashTable{pformat(entries)[1:-1]}\n{' ...' if self.len >= 20 else ''}}})"
+
--- test_chash_table.py
+++ test_chash_table.py
@@ -8,6 +8,13 @@ def test_bucket_resize(ht):
     ht = HashTable(c_int64, {'data': c_int64})
     for i in range(100):
         ht[2] = {'data': 2}
+    print(ht)
+
+def test_with_vatying_data():
+    ht = HashTable(c_wchar_p, {'data': c_int64})
+    for i in range(100):
+        ht['test' + str(i)] = {'data': i}
+    print(ht)
 
 def test_with_reads(ht):
     ht = HashTable(c_int64, {'data': c_int64})
@@ -16,6 +23,7 @@ def test_with_reads(ht):
         x = ht[2]
 
 test_bucket_resize(ht)
+test_with_vatying_data()
 test_with_reads(ht)
 
 
